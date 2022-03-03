644fe414ccdcae5c737ac9e98c95b8b83ea190e3
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-03 11:36:46 -0500

    update app file to include Layout + scss globals

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
+import React from "react";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 
-import '../styles/globals.css';
+import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <React.Fragment>
+    <Layout>
       <Component {...pageProps} />
-    </React.Fragment>
-  )
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
