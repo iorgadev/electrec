81e4a705d2f13588d51493d7b26e434bb64b36e3
iorgadev <2624432+iorgadev@users.noreply.github.com>
2022-03-04 11:15:08 -0500

    add initial context states

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,8 +1,14 @@
-import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
-
 import "../styles/globals.scss";
+
+import { atom } from "jotai";
+export const isRecordingAtom = atom(false);
+export const sourceAtom = atom<Electron.DesktopCapturerSource | null>(null);
+
+export const startRecording = (mediaRecorder) => {
+  mediaRecorder.start();
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
